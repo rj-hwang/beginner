18f6ff689770a30f41e903e628e31a561b7446a3
RJ.Hwang <rongjihuang@gmail.com>
2017-12-12 09:17:31 +0800

    Polishing

--- src/main/java/com/beginner/jpa/ActorType.java
+++ src/main/java/com/beginner/jpa/ActorType.java
@@ -1,26 +1,26 @@
 package com.beginner.jpa;
 
 /**
- * @author dragon 2016-01-01
+ * @author RJ
  */
 public enum ActorType {
-	User(1),
-	Group(1 << 1),
-	Department(1 << 2);
+  User(1),
+  Group(1 << 1),
+  Department(1 << 2);
 
-	private final Integer value;
+  private final Integer value;
 
-	ActorType(Integer value) {
-		this.value = value;
-	}
+  ActorType(Integer value) {
+    this.value = value;
+  }
 
-	public Integer getValue() {
-		return value;
-	}
+  public Integer getValue() {
+    return value;
+  }
 
 
-	@Override
-	public String toString() {
-		return "AT:" + super.toString();
-	}
+  @Override
+  public String toString() {
+    return "AT:" + super.toString();
+  }
 }
--- src/main/java/com/beginner/jpa/ActorTypeConverter.java
+++ src/main/java/com/beginner/jpa/ActorTypeConverter.java
@@ -5,21 +5,22 @@ import javax.persistence.Converter;
 
 /**
  * 指定 autoApply = true 后, 就不需要在实体类的属性上声明 @Convert 注解指定转换器了.
- * @author dragon 2016-01-01
+ *
+ * @author RJ
  */
 @Converter(autoApply = true)
 public class ActorTypeConverter implements AttributeConverter<ActorType, Integer> {
-	@Override
-	public Integer convertToDatabaseColumn(ActorType actorType) {
-		return actorType == null ? null : actorType.getValue();
-	}
+  @Override
+  public Integer convertToDatabaseColumn(ActorType actorType) {
+    return actorType == null ? null : actorType.getValue();
+  }
 
-	@Override
-	public ActorType convertToEntityAttribute(Integer value) {
-		if (value == null) return null;
-		for (ActorType type : ActorType.values()) {
-			if (type.getValue().equals(value)) return type;
-		}
-		throw new IllegalArgumentException("No ActorType for value " + value);
-	}
+  @Override
+  public ActorType convertToEntityAttribute(Integer value) {
+    if (value == null) return null;
+    for (ActorType type : ActorType.values()) {
+      if (type.getValue().equals(value)) return type;
+    }
+    throw new IllegalArgumentException("No ActorType for value " + value);
+  }
 }
