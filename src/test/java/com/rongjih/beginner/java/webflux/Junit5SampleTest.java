058e7a10832e41f2e3bb9e311094ff8c6b293c1c
RJ.Hwang <rongjihuang@gmail.com>
2017-12-29 00:14:47 +0800

    Config Junit5

---
+++ src/test/java/com/rongjih/beginner/java/webflux/Junit5SampleTest.java
@@ -0,0 +1,15 @@
+package com.rongjih.beginner.java.webflux;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+/**
+ * @author RJ
+ */
+class Junit5SampleTest {
+  @Test
+  void test() {
+    assertEquals(2, 1 + 1);
+  }
+}
